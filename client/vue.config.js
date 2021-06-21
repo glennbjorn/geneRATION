46f4962977351194ccecae8551305702adc7f242
glennbjorn <glenn.bjorn.99@gmail.com>
2021-06-21 21:17:37 +0800

    Minor changes

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,16 +1,36 @@
 const path = require('path');
 
-module.exports = {
-    outputDir: path.resolve(__dirname, '../server/public'),
-    devServer: {
-        proxy: {
-            '^/api': {
-                target: 'http://localhost:4000',
-                changeOrigin: true,
-                secure:false,
-                pathRewrite: {'^/api': '/'},
-                logLevel: 'debug' 
-            },
+if (process.env.NODE_ENV === "development") {
+    module.exports = {
+        outputDir: path.resolve(__dirname, '../server/public'),
+        devServer: {
+            proxy: {
+                '^/api': {
+                    target: 'http://localhost:4000',
+                    changeOrigin: true,
+                    secure:false,
+                    pathRewrite: {'^/api': '/'},
+                    logLevel: 'debug' 
+                },
+            }
         }
     }
 }
+
+if (process.env.NODE_ENV === "production") {
+    module.exports = {
+        outputDir: path.resolve(__dirname, '../server/public'),
+        devServer: {
+            proxy: {
+                '^/api': {
+                    target: 'https://new-generation.herokuapp.com',
+                    changeOrigin: true,
+                    secure:false,
+                    pathRewrite: {'^/api': '/'},
+                    logLevel: 'debug' 
+                },
+            }
+        }
+    }
+}
+
