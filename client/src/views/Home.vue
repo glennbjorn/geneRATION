4c9eda6230d0f70e225e7b39aefc4b7bb07602a7
yueyin22 <e0556336@u.nus.edu>
2021-06-25 13:41:54 +0800

    Edited width of paragraph(web): Home

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -8,31 +8,31 @@
     <a href="#ongoing-campaign"> Ongoing Campaigns </a>
 
     <span class="generation-intro">
-      <h2>geneRATION is your a one-stop web platform that eases the process of organising food drives for charitable organisations</h2>
+      <h2>a one-stop <br>food drive organiser<br></h2>
     </span>
 
   <h2 id="section-aboutus"> About Us </h2>
   <div class="aboutus-text"> 
     <p> When it comes to food drives, there are always bumps in the process of soliciting donations
-        from the public as well as the selection, sorting, and distribution of food products.</p>
+        from the public <br>as well as the selection, sorting, and distribution of food products.<br></p>
 
-    <p> One of the problems is a shortage of manpower to go door-to-door and engage with
-        members of the public in attempt to prompt them to contribute to the food drive. There is
+    <p> <br>One of the problems is a shortage of manpower to go door-to-door and engage with
+        members of the public <br>in attempt to prompt them to contribute to the food drive.<br> There is
         also a manpower shortage to visit each unit a week later to check if would like to donate. </p>
 
-    <p> Since volunteers have no clue which units would be donating, the conventional method
+    <p> <br>Since volunteers have no clue which units would be donating, the conventional method
         would be to knock on every unit. </p>
 
-    <p> Other problems include a lack of food donations to evenly allocate to the beneficiaries,
-        leading to the charitable organisations spending their own money to supplement these
-        missing products. </p> 
+    <p> <br>Other problems include a lack of food donations to evenly allocate to the beneficiaries,
+        <br>leading to the charitable organisations spending their own money to supplement these
+        missing products.<br> </p> 
 
-    <p> Also, food products that are donated must be re-screened to ensure that
-        they are halal certified and have a minimum of three months left on their shelf life. </p>
+    <p> <br>Also, food products that are donated must be re-screened to ensure that
+        they are halal certified <br>and have a minimum of three months left on their shelf life.<br> </p>
 
-        As a result, as individuals who recognise the value of food donation drives and their effect
-        on the less fortunate in our society, we want to build a website that will make it easier for
-        charitable organisations to organise food drives in the future. </div>
+        <br>As a result, as individuals who recognise the value of food donation drives and their effect
+        on the less fortunate <br>in our society, we want to build a website that will make it easier for
+        charitable organisations<br> to organise food drives in the future. </div>
 
   <h3 id="section-howitworks"> How it Works </h3>
   <div class="howitworks-text"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, aliquam? </div>
@@ -90,10 +90,10 @@ export default {
 
 .logo {
 display: block;
-margin-left: auto;
+margin-left: 70px;
 margin-right: auto;
-float: right;
-margin-top: 100px; 
+float: left;
+margin-top: 50px; 
 }
 
 .page {
@@ -105,9 +105,10 @@ margin-top: 100px;
 .generation-intro h2 {
   font-family: 'Poiret One', cursive;
   font-weight: bolder;
-  color: #c42218;
+  /* color: #c42218; */
+  color: #de184d;
   text-align: center;
-  margin-top: 100px;
+  margin-top: 150px;
   font-size: 80px;
   margin-bottom: 30px;
 }
@@ -117,7 +118,7 @@ font-family: 'Poiret One', cursive;
   font-weight: bolder;
   color: #4a0602;
   text-align: center;
-  margin-top: 100px;
+  margin-top: 200px;
   font-size: 50px;
   margin-bottom: 30px;
 }
