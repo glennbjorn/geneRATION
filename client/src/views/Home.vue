4d85b77bb096107ba4fa653a88fecc0686b2c434
yueyin22 <e0556336@u.nus.edu>
2021-06-25 15:09:04 +0800

    Edited Home

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,9 +3,10 @@
   <div class="page">
 
     <img class="logo" src="@/assets/logo.png" alt="Logo of geneRATION">
-    <a href="#section-aboutus"> About Us </a>
-    <a href="#section-howitworks"> How it Works </a>
-    <a href="#ongoing-campaign"> Ongoing Campaigns </a>
+    
+      <a href="#section-aboutus"> About Us </a>&emsp;
+    <a href="#section-howitworks"> How it Works </a>&emsp;
+    <a href="#ongoing-campaign"> Ongoing Campaigns </a>&emsp;
 
     <span class="generation-intro">
       <h2>a one-stop <br>food drive organiser<br></h2>
@@ -13,26 +14,26 @@
 
   <h2 id="section-aboutus"> About Us </h2>
   <div class="aboutus-text"> 
-    <p> When it comes to food drives, there are always bumps in the process of soliciting donations
-        from the public <br>as well as the selection, sorting, and distribution of food products.<br></p>
+    <p class="para-space"> When it comes to food drives, there are always bumps in the process of soliciting donations
+        from the public as well as the selection, sorting, and distribution of food products.</p>
 
-    <p> <br>One of the problems is a shortage of manpower to go door-to-door and engage with
-        members of the public <br>in attempt to prompt them to contribute to the food drive.<br> There is
+    <p class="para-space"> One of the problems is a shortage of manpower to go door-to-door and engage with
+        members of the public in attempt to prompt them to contribute to the food drive. There is
         also a manpower shortage to visit each unit a week later to check if would like to donate. </p>
 
-    <p> <br>Since volunteers have no clue which units would be donating, the conventional method
+    <p class="para-space"> Since volunteers have no clue which units would be donating, the conventional method
         would be to knock on every unit. </p>
 
-    <p> <br>Other problems include a lack of food donations to evenly allocate to the beneficiaries,
-        <br>leading to the charitable organisations spending their own money to supplement these
-        missing products.<br> </p> 
+    <p class="para-space"> Other problems include a lack of food donations to evenly allocate to the beneficiaries,
+        leading to the charitable organisations spending their own money to supplement these
+        missing products.</p> 
 
-    <p> <br>Also, food products that are donated must be re-screened to ensure that
-        they are halal certified <br>and have a minimum of three months left on their shelf life.<br> </p>
+    <p class="para-space"> Also, food products that are donated must be re-screened to ensure that
+        they are halal certified and have a minimum of three months left on their shelf life.</p>
 
-        <br>As a result, as individuals who recognise the value of food donation drives and their effect
-        on the less fortunate <br>in our society, we want to build a website that will make it easier for
-        charitable organisations<br> to organise food drives in the future. </div>
+        As a result, as individuals who recognise the value of food donation drives and their effect
+        on the less fortunate in our society, we want to build a website that will make it easier for
+        charitable organisations to organise food drives in the future. </div>
 
   <h3 id="section-howitworks"> How it Works </h3>
   <div class="howitworks-text"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, aliquam? </div>
@@ -129,8 +130,13 @@ text-align: center;
   font-size: 30px;
   font-weight: bold;
   margin-bottom: 30px;
+  margin-left: 100px;
+  margin-right: 100px;
 }
 
+.para-space {
+  margin-bottom: 50px;
+}
 #section-howitworks {
 font-family: 'Poiret One', cursive;
   font-weight: bolder;
