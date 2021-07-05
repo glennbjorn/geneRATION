e1ea5683ba6ef0c0af3f17e8fd38c4deec204abb
yueyin22 <e0556336@u.nus.edu>
2021-07-05 22:05:49 +0800

    Form.vue: Function created to get values of textbox and storing it inside a array (quantity_arr)

--- client/src/views/Form.vue
+++ client/src/views/Form.vue
@@ -56,21 +56,59 @@
         </div>
 
         <!-- <div class="items-checkbox">
+          <p  class="all" >Select all</p>
           <input v-model="toggle" type="checkbox" @click="selectAll" />
-          <p>Select all</p>
-        </div> -->
+        </div>-->
 
-        <div class="items-checkbox" :key="n" v-for="n in donor.items.length">
+        <!-- <div class="items-checkbox" :key="n" v-for="n in donor.items.length">
           <input
             v-model="donor.items[n - 1].donate"
             type="checkbox"
             id="items"
           />
           <p>
-            <!-- {{ campaign.items[n - 1].qty }} x {{ campaign.items[n - 1].item }} -->
-            {{ campaign.items[n - 1].item }}
+            {{ campaign.items[n - 1].qty }} x {{ campaign.items[n - 1].item }}
           </p>
-        </div>
+        </div>-->
+
+        <table class="table table-borderless">
+          <!-- <thead>
+            <tr>
+              <th scope="col">Select All</th>
+              <th scope="col">
+                <input v-model="toggle" type="checkbox" @click="selectAll" />
+              </th>
+            </tr>
+          </thead> -->
+          <tbody>
+            <tr>
+              <th colspan="1">Item Name</th>
+              <th scope="col">Quantity</th>
+            </tr>
+            <tr :key="n" v-for="n in donor.items.length">
+              <td>{{ campaign.items[n - 1].item }}</td>
+              <!-- <td>
+                <input
+                  v-model="donor.items[n - 1].donate"
+                  type="checkbox"
+                  id="items"
+                />
+              </td> -->
+              <!-- <td>
+                <input
+                  v-model="campaign.items[n - 1].qty"
+                  type="number"
+                  min="1"
+                  value="1"
+                  name="quantity"
+                />
+              </td> -->
+              <td>
+                <input type="number" value="1" name="quantity" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
 
         <div class="mini-header">
           <h5>Declarations</h5>
@@ -86,7 +124,7 @@
 
         <div class="agree-checkbox">
           <label for="halal"
-            >I agree to only donate items that are <b>Halal Certified</b></label
+            >I agree to only donate items that are <b>Halal certified</b></label
           >
           <input v-model="donor.halal" type="checkbox" id="halal" />
         </div>
@@ -103,6 +141,7 @@
 
         <button class="donate" type="submit">Pledge My Donation!</button>
       </form>
+      <!--<button class="back" @click="$router.push(`/${campaignid}`)">-->
       <button class="back" @click="$router.push(`/${campaignid}`)">
         Back to Campaigns
       </button>
@@ -114,8 +153,21 @@
 import axios from "axios";
 import router from "@/router";
 import Nav from "../components/Nav.vue";
+var quantity = null;
+var quantity_arr = [];
+
+//To get values of the textbox and store inside an array (quantity_arr)
+function update_quantity() {
+  quantity = document.getElementsByName("quantity");
+  for (var x = 0; x < quantity.length; x++) {
+    //store in array
+    quantity_arr.push(quantity[x].value);
+  }
+}
+
 export default {
   name: "Form",
+
   data() {
     return {
       campaignid: "",
@@ -134,59 +186,90 @@ export default {
       isLoading: true,
     };
   },
+
   components: {
     Nav,
   },
+
   methods: {
     getCampaignId() {
       this.campaignid = localStorage.getItem("formId");
     },
+
     async getCampaign() {
       const res = await axios.post("/api/campaign/getCampaignById", {
         _id: this.campaignid,
       });
+
       const data = await res.data[0];
+
       return data;
     },
     getItems() {
       for (let i = 0; i < this.campaign.items.length; i++) {
         var item = this.campaign.items[i].item;
-        var data = { item: item, donate: false };
+        quantity = this.campaign.items[i].qty;
+        var data = { item: item, quantity: quantity, donate: false };
         this.donor.items = [...this.donor.items, data];
       }
     },
+
     async submit() {
+      // Call function to get the updated array of the quantity,
+      // Empty the donor array to add new/updated quantity,
+      // Reuse the code on getItems() but change to use quantity_arr instead of campaign quantity ("update quantity")
+      // Donate change to true
+
+      // get updated array of quantity
+      update_quantity();
+      //empty the donor item array to store updated quantity
+      this.donor.items = [];
+      console.log(quantity_arr);
+      for (let i = 0; i < this.campaign.items.length; i++) {
+        var item = this.campaign.items[i].item;
+        quantity = quantity_arr[i];
+        var data = { item: item, quantity: quantity, donate: true };
+        this.donor.items = [...this.donor.items, data];
+      }
+
       if (!this.donor.name) {
         this.$swal("Please include your name!");
         return;
       }
+
       if (!this.donor.contact) {
         this.$swal("Please include your contact number!");
         return;
       }
+
       if (!this.donor.address) {
         this.$swal("Please include your postal code!");
         return;
       }
+
       if (this.donor.address.length != 6) {
         this.$swal("Your postal code should be 6 digits!");
       }
+
       if (!this.donor.unit) {
         this.$swal("Please include your unit number!");
         return;
       }
+
       if (!this.donor.shelfLife) {
         this.$swal(
           "Please declare that you agree to donate items with sufficient shelf life!"
         );
         return;
       }
+
       if (!this.donor.halal) {
         this.$swal(
           "Please declare that you agree to donate items that are Halal!"
         );
         return;
       }
+
       await axios.post("/api/donate", {
         campaignid: this.campaignid,
         name: this.donor.name,
@@ -198,14 +281,17 @@ export default {
       });
       localStorage.removeItem("tqid");
       localStorage.setItem("tqid", this.campaignid);
+
       router.push("/ThankYou");
     },
+
     selectAll() {
       for (let i = 0; i < this.donor.items.length; i++) {
         this.donor.items[i].donate = !this.toggle;
       }
     },
   },
+
   async created() {
     this.getCampaignId();
     this.campaign = await this.getCampaign();
@@ -220,42 +306,63 @@ export default {
   text-align: center;
   margin-top: -20px;
 }
+
 .form-page {
   width: 100%;
   max-width: 700px;
   padding: 15px;
   margin: auto;
 }
+
 .form-page .form-form {
   display: flex;
   flex-direction: column;
   padding: 15px;
 }
+.all {
+  display: inline;
+}
+.item-name {
+  display: inline;
+  margin-left: 15px;
+}
+
 .items-checkbox {
   width: 50%;
   margin-left: 15px;
 }
-.items-checkbox input {
+
+.items-checkbox-quantity {
   float: right;
-  transform: scale(1.5);
+  margin-right: 25px !important;
 }
+
+.items-checkbox input {
+  transform: scale(1.5);
+  float: right;
+}
+
 .agree-checkbox {
   margin-top: 20px;
   margin-left: 15px;
 }
+
 .agree-checkbox input {
   float: right;
   transform: scale(1.5);
   margin-right: 15px;
 }
+
 .mini-header {
   margin-top: 30px;
   margin-bottom: 20px;
   margin-left: 15px;
 }
+
 .mini-header h5 {
   text-align: left;
 }
+
 .donate {
   background: white;
   width: 100%;
@@ -265,6 +372,7 @@ export default {
   cursor: pointer;
   margin-top: 20px;
 }
+
 .back {
   background: white;
   border-inline: 3px;
