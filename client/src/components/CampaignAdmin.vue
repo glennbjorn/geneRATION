3c2c01da68689cb0e558e5d22154de2ad43e4533
glennbjorn <77476786+glennbjorn@users.noreply.github.com>
2021-08-16 16:19:15 +0800

    Minor edits

--- client/src/components/CampaignAdmin.vue
+++ client/src/components/CampaignAdmin.vue
@@ -18,7 +18,7 @@
     <div>
       <p><b>Collection Date:</b> {{ date }}</p>
       <p>
-        <b>Self Drop-Off Location:</b><br />
+        <b>Location(s):</b><br />
         {{ campaignAdmin.collectionAddress }}
       </p>
     </div>
